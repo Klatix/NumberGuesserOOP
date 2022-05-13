ab35c3e0d394f4a58ccfcf1e686b78917c6a91fe
Klatix <75433937+Klatix@users.noreply.github.com>
2022-05-14 01:23:39 +0200

    Update Program.cs

--- BasicNumberGuesser/Program.cs
+++ BasicNumberGuesser/Program.cs
@@ -9,7 +9,7 @@ namespace BasicNumberGuesser
         {
             //version display
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Number Guesser App in C# v.1.1, created by Klatix");
+            Console.WriteLine("Number Guesser App in C# v.1.2, created by Klatix");
             Console.ResetColor();
 
             //creating objects necessary to run the game
